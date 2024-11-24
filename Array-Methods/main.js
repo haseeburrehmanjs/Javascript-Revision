b0d5ae24e3669abb0db0ee7335995d27172619eb
haseeburrehmanjs <kamranking786500@gmail.com>
2024-11-24 22:18:59 +0500

    array-pratice

--- Array-Methods/main.js
+++ Array-Methods/main.js
@@ -1,12 +1,12 @@
-console.log('hello world i am javascript');
+console.log("hello world i am javascript");
 
-// push method === array ka last ma add krna ka liya 
+// push method === array ka last ma add krna ka liya
 // unshift method === array ka start ma add krna ka liya
 // length method === array ki length malom krna ka liya
 // pop methodh === array ka last sa remove krna ka liya
-// unshift method === array ka start sa remove krna ka liya 
+// unshift method === array ka start sa remove krna ka liya
 
-const softwear = ['Photoshop', 'Figma', 'In Desgin', 'Corel Draw']
+const softwear = ["Photoshop", "Figma", "In Desgin", "Corel Draw"];
 
 // console.log(softwear.length);
 
@@ -20,3 +20,27 @@ const softwear = ['Photoshop', 'Figma', 'In Desgin', 'Corel Draw']
 // softwear.shift()
 
 console.log(softwear);
+
+const form = document.querySelector("form");
+const inputValue = document.querySelector("#todoValue");
+const list = document.querySelector("#list");
+
+const myTodos = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(inputValue.value);
+  myTodos.push(inputValue.value);
+  console.log(myTodos);
+  renderScreen()
+  inputValue.value = "";
+});
+
+const renderScreen = () => {
+    list.innerHTML = ''
+  myTodos.map((item, index) => {
+    list.innerHTML += `
+        <li>${item}</li>
+        `
+  });
+};
