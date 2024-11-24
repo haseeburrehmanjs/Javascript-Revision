020e7db7301ee1f1044f1fb16bf211109c582029
haseeburrehmanjs <kamranking786500@gmail.com>
2024-11-24 21:38:45 +0500

    project-initialzed

---
+++ Array-Methods/main.js
@@ -0,0 +1,22 @@
+console.log('hello world i am javascript');
+
+// push method === array ka last ma add krna ka liya 
+// unshift method === array ka start ma add krna ka liya
+// length method === array ki length malom krna ka liya
+// pop methodh === array ka last sa remove krna ka liya
+// unshift method === array ka start sa remove krna ka liya 
+
+const softwear = ['Photoshop', 'Figma', 'In Desgin', 'Corel Draw']
+
+// console.log(softwear.length);
+
+//  softwear.push('primear pro')
+//  console.log(softwear);
+
+//  softwear.unshift('primear pro')
+//  console.log(softwear);
+
+// softwear.pop()
+// softwear.shift()
+
+console.log(softwear);
