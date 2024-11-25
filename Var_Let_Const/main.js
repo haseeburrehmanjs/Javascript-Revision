419339469e603a5d7f0ffa28397b911b4a782335
haseeburrehmanjs <kamranking786500@gmail.com>
2024-11-25 21:31:08 +0500

    let_const_var

---
+++ Var_Let_Const/main.js
@@ -0,0 +1,24 @@
+// var === ka variable ko hum same name sa redeclare bhi kr sakta hain or reassgin bhi 
+// let === ka variable ko hum same name sa redeclare nahi kr sakhta but reassgin kr sakhta hain 
+// const === ka varibale ko hum same name sa na redeclare kr sakhta hain na hi value reassgin
+
+// var user_name = 'haseeb ur rehman js'
+// var user_name = 'kamran ahmed' 
+// var user_name = 'muhammad asharib'
+
+// console.log(user_name);
+
+// ==================================================================
+
+// let fav_Number = 8
+// let fav_Number = 9 === 'fav_Number' has already been declared.
+// fav_Number = 10 reassgin successfully
+
+// console.log(fav_Number);
+
+// ==================================================================
+
+const collage_Name = 'jinnah government collage karachi'
+// const collage_Name = 'jinnah government collage karachi' 'collage_Name' has already been declared.
+
+console.log(collage_Name);
